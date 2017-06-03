6aac8e26ad4ac76851913b2aef635b4eb6674145
wesleyhuang23 <wesleyhuang23@gmail.com>
2017-06-02 21:28:36 -0700

    added comments

--- script.js
+++ script.js
@@ -4,12 +4,12 @@ angular.module('app', ['ui.router']).config(function ($urlRouterProvider, $state
     $stateProvider
         .state('user', {
             templateUrl: './user.html',
-            url: '/:name',
+            url: '/:name', //params of the user
             controller: 'userCtrl'
         });
 
 })
-
+//controller for overall app
 .controller('mainCtrl', function($scope, mainSvc, $location){
     
   let getPeople = () => {
@@ -22,7 +22,6 @@ angular.module('app', ['ui.router']).config(function ($urlRouterProvider, $state
   getPeople(); //initial data request
 
   $scope.setContact = function(people){ //seting the first person in the route
-      console.log($scope.people);
       $location.url('/' + people[0].name);
   } 
 
@@ -31,29 +30,31 @@ angular.module('app', ['ui.router']).config(function ($urlRouterProvider, $state
   }
 
 })
+//controller for contact details
 .controller('userCtrl', function($scope, $stateParams){
 
-    let person = $scope.people.filter((person) => {
+    let person = $scope.people.filter((person) => { //using name to filter down to specific person to show in details
         return person.name === $stateParams.name;
     })
+
+    starRating(person[0].rating)
     
-    function starRating(rating){
+    function starRating(rating){ //logic to set rating with stars
         let blankStar = '☆';
         let star = '★';
         let ratings = [];
         for(let i = 0; i < rating; i++){
-            ratings.push({icon: star, id: i});
+            ratings.push({icon: star, id: i}); //making object so id can be used to avoid duplicates
         }
         let x = ratings.length
         while(x < 5){
             ratings.push({icon: blankStar, id: x});
             x++;
         }
-        
         person[0].stars = ratings;
     }
-    starRating(person[0].rating)
-    $scope.user = person[0];
+    
+    $scope.user = person[0]; //setting scope to the filtered user.
 })
 //service
 .service('mainSvc', function($http){
